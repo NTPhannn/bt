d10a63b11f655c1638aeffdce0de195a7bc365f1
NTPhannn <158154267+NTPhannn@users.noreply.github.com>
2024-03-04 06:27:24 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function login() {
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+
+  // Perform login logic here
+  console.log('Login with username: ' + username + ' and password: ' + password);
+}
+
+function register() {
+  var newUsername = document.getElementById('newUsername').value;
+  var newPassword = document.getElementById('newPassword').value;
+
+  // Perform registration logic here
+  console.log('Register with username: ' + newUsername + ' and password: ' + newPassword);
+}
